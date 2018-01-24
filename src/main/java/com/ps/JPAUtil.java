8217650c84493d7549234382940911dc7f8b1977
prerohan <prerohan@9.84.228.43>
2018-01-24 19:41:01 +0530

    FiRST

---
+++ src/main/java/com/ps/JPAUtil.java
@@ -0,0 +1,32 @@
+package com.ps;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.xml.ws.ServiceMode;
+
+@Named
+public class JPAUtil implements Serializable {
+	private static final String PERSISTENCE_UNIT_NAME = "K-HATCHARY";
+	private static EntityManagerFactory factory;
+
+	@Produces
+	@ApplicationScoped
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		return factory;
+	}
+
+	public static void shutdown() {
+		if (factory != null) {
+			factory.close();
+		}
+	}
+}
---
+++ src/main/java/com/ps/entity/UserType.java
@@ -0,0 +1,5 @@
+package com.ps.entity;
+
+public enum UserType {
+ADMINS,SELLS,PRODUCTION,PROCUREMENT;
+}
